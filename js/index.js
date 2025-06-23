3079a36092d8fbc0af8e8e987eeb9e18504154d3
daehyeong2 <baconbacon1231@gmail.com>
2025-06-23 23:44:59 +0900

    feat: github api 연동 -> 새싹 크기 자동 조절 (배열에 반별로 gh id 수동 입력 필요)

--- js/index.js
+++ js/index.js
@@ -1,30 +1,104 @@
-scores = {
-    1: 19000,
-    2: 10000,
-    3: 5000,
-    4: 1000
+import env from "/js/env.js";
+
+const local_cache = localStorage.getItem("gh-cache");
+const cache = local_cache ? JSON.parse(local_cache) : {};
+
+const scores = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
 }
 
-// const browser = await puppeteer.launch();
-// const page = await browser.newPage();
-//
-// const url = "https://github.com/daehyeong2";
-//
-// async function fetchData() {
-//     try {
-//         await page.goto(url);
-//         console.log(await page.locator("#js-contribution-activity-description"))
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//     }
-// }
-// fetchData();
+const classmate = [
+    // 1반 Github Ids
+    [
+        "daehyeong2",
+        "Finefinee"
+    ],
+    // 2반 Github IDs
+    [
 
-setTimeout(() => {
-    sprouts = document.getElementsByClassName("sprout");
-    for(let i = 0; i < 4; i++){
-        sprouts[i].style.scale = (1 + (scores[i+1] * 0.01)).toString();
+    ],
+    // 3반 Github IDs
+    [
+
+    ],
+    // 4반 Github IDs
+    [
+
+    ]
+]
+
+const data = {};
+
+const headers = {
+    Authorization: `Bearer ${env.GHP_TOKEN}`
+}
+
+const query = `
+    query($login: String!) {
+        user(login: $login) {
+            contributionsCollection {
+                contributionCalendar {
+                    totalContributions
+                }
+            }
+        }
     }
-}, 300);
+`
 
-await browser.close();
+const get_contributions = async (username) => {
+    if(cache[username]) {
+        return cache[username].contributions
+    }
+    console.log("new request");
+    const res = await fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+            query,
+            variables: {
+                login: username
+            }
+        }),
+    })
+    if(!res.ok) alert("깃허브 데이터를 불러오는 중 오류가 발생했습니다.");
+    const body = await res.json();
+    const contributions = body.data.user?.contributionsCollection?.contributionCalendar?.totalContributions;
+    if(contributions == null) alert("깃허브 데이터를 불러오는 중 오류가 발생했습니다.");
+    cache[username] = {
+        contributions,
+        expiredAt: new Date().getTime() + (1000 * 60 * 60)
+    };
+    return contributions
+}
+
+async function fetchData() {
+    try {
+        for(let i = 0; i < 4; i++){
+            const cls = classmate[i];
+            for(let stuIdx = 0; stuIdx < cls.length; stuIdx++){
+                if(!cls[stuIdx]) continue;
+                data[cls[stuIdx]] = await get_contributions(cls[stuIdx]);
+                scores[i+1] += data[cls[stuIdx]];
+            }
+        }
+        const sprouts = document.getElementsByClassName("sprout");
+        for(let i = 0; i < 4; i++){
+            sprouts[i].style.scale = (1 + (scores[i+1] * 0.003)).toString();
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+fetchData();
+
+setInterval(() => {
+    const newCache = {}
+    for(let c in cache){
+        const ca = cache[c];
+        if(ca.expiredAt > new Date().getTime()) newCache[c] = cache[c];
+    }
+    localStorage.setItem("gh-cache", JSON.stringify(newCache));
+}, 1000);
