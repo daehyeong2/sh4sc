5445f428f25120492d67c1c057e3fa787b423054
daehyeong2 <baconbacon1231@gmail.com>
2025-06-24 18:00:22 +0900

    feat: 팝업 페이지 수정

--- js/popup_login.js
+++ js/popup_login.js
@@ -1,16 +1,22 @@
-const form = document.getElementById("input-id");
+const form = document.querySelector("form");
 const username = document.getElementById("username");
 const button_close = document.getElementById("close");
 
-const onSubmit = () => {
+const onSubmit = (e) => {
+    e.preventDefault();
+    if(username.value == "") {
+        alert("값을 입력해 주세요.");
+        return;
+    }
+    localStorage.setItem("username", username.value);
     alert(`${username.value}님, 환영합니다!`)
-    window.close()
+    parent.window.location.reload();
 }
 
 form.addEventListener("submit", onSubmit);
 
 const onClose = () => {
-    window.close()
+    parent.document.querySelector("iframe").remove();
 }
 
 button_close.addEventListener("click", onClose)
